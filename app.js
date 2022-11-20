827bdd888c7a59aab7cb92b25f5ca08980018b6a
Sennthx <totharon1173@gmail.com>
2022-11-20 15:24:49 +0100

    started to add functionality to buttons and input fields

---
+++ app.js
@@ -0,0 +1,22 @@
+// Getting input elements
+const errorMsg = document.querySelector("#error-msg");
+const loanInput = document.querySelector("#loanInput");
+const interestInput = document.querySelector("#interestInput");
+const yearInput = document.querySelector("#yearInput");
+const calculate = document.querySelector("#calculate");
+
+// Getting output elements
+const loading = document.querySelector(".loading");
+const results = document.querySelector(".results");
+const outputMonthly = document.querySelector("#outputMonthly");
+const outputPayment = document.querySelector("#outputPayment");
+const outputInterest = document.querySelector("#outputInterest");
+
+// Loading action Listeners
+
+calculate.addEventListener("click", calculate());
+
+function calculate(){
+    
+    
+}
